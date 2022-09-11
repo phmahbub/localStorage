2b87b2b7b6933aa9b0b623de144beee1579bed46
B. M. Mahbubul Alam <108087360+phmahbub@users.noreply.github.com>
2022-09-11 23:39:52 +0600

    a search box is added and search data is shown on DOM....boom

--- local.js
+++ local.js
@@ -78,17 +78,17 @@ document.getElementById('add-button-all').addEventListener('click', function(){
         
     }
     displayData()
-    location.reload()
+    reload()
 })
 
 document.getElementById('delete-btn-all').addEventListener('click', function(){
     localStorage.clear()
-    location.reload()
+    reload()
 
 })
 
 //display data 
-// console.log(JSON.parse(getData('studentInfo'))[0].age)
+
 const displayData = ()=>{
     const studentInformation = JSON.parse(localStorage.getItem('studentInfo'))
     if(studentInformation){
@@ -101,11 +101,8 @@ const displayData = ()=>{
             <td>${student.lname}</td>
             <td>${student.age}</td>
         </tr>
-    `;
+    `;});}}
     
-        });
-    }
-}
 displayData()
 
 //remove item one by one from the array
@@ -113,25 +110,52 @@ document.getElementById('remove-last-button').addEventListener('click', function
     const student = JSON.parse(localStorage.getItem('studentInfo'))
     student.pop()
     localStorage.setItem('studentInfo', JSON.stringify(student))
-      location.reload()
+      reload()
 })
 
 // search data in a table (its not working in this local storage ..... need some extra work to activate it )
-document.getElementById('inputGroup-sizing-lg').addEventListener('click', function(){
-    console.log('cat')
-    const searchFieldValue = document.getElementById('search-field').value.toUpperCase()
-    const table = document.getElementById('table')
-    const tr = table.getElementsByTagName('tr')
-    for(let i = 0; i < tr.length; i++){
-        let td = tr[i].getElementsByTagName('td')[0];
-        if(td){
-            let textValue = td.textContent || td.innerHTML;
-            if(textValue.toUpperCase().indexOf(searchFieldValue)>-1){
-                tr[i].style.display = '';
-            } else{
-                tr[i].style.display = 'none';
-            }
-        }
-    }
+// document.getElementById('inputGroup-sizing-lg').addEventListener('click', function(){
+//     console.log('cat')
+//     const searchFieldValue = document.getElementById('search-field').value.toUpperCase()
+//     const table = document.getElementById('table')
+//     const tr = table.getElementsByTagName('tr')
+//     for(let i = 0; i < tr.length; i++){
+//         let td = tr[i].getElementsByTagName('td')[0];
+//         if(td){
+//             let textValue = td.textContent || td.innerHTML;
+//             if(textValue.toUpperCase().indexOf(searchFieldValue)>-1){
+//                 tr[i].style.display = '';
+//             } else{
+//                 tr[i].style.display = 'none';
+//             }
+//         }
+//     }
 
-})
+// })
+
+
+
+
+// New Search 
+document.getElementById("search-button").addEventListener("click", function () {
+    const inputField = document.getElementById("search-field").value.toLowerCase();
+    const data = JSON.parse(localStorage.getItem('studentInfo'))
+    let results = []
+    for(let i=0; i<data.length; i++) {
+        for(key in data[i]) {
+          if(data[i][key].toLowerCase().indexOf(inputField)!=-1) {
+            results.push(data[i]);
+            const dataContainer = document.getElementById('tbody2')
+        // dataContainer.innerHTML = ''
+        if(inputField !== ''){
+            dataContainer.innerHTML += `
+            <tr>
+                <td>${results[0].fname}</td>
+                <td>${results[0].lname}</td>
+                <td>${results[0].age}</td>
+            </tr>
+        `; }}}}});
+
+function reload(){
+    location.reload()
+}
